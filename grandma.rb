0fad46ecd055003dfd1961cd74082c538dfcbb6d
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-05 19:31:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(words)
   if(words=="I LOVE YOU GRANDMA!")
-    "I LOVE YOU TOO, PUMPKIN"
+  puts "I LOVE YOU TOO, PUMPKIN"
   elsif(words==words.upcase)
-  "NO, NOT SINCE 1938!"
+  puts "NO, NOT SINCE 1938!"
   else
-  "HUH?! SPEAK UP, SONNY!"
+ puts "HUH?! SPEAK UP, SONNY!"
 end
