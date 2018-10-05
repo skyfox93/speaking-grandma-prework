61556c3f16cdcea2db6ca09594ecc4f57035ddff
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-05 19:29:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(words)
     "I LOVE YOU TOO, PUMPKIN"
   elsif(words==words.upcase)
   "NO, NOT SINCE 1938!"
-else
+  else
   "HUH?! SPEAK UP, SONNY!"
 end
 # Whatever you say to grandma, she should respond with
